03560cb8ec4c46357de9b3fd450c30fe9abdde01
J.Zirena <JoshZirena@gmail.com>
2018-08-02 18:34:13 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/',(req, res) => {
+  res.render('home.hbs', {
+    siteTitle: 'Josh Zirena',
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my first nodeJS powered by Express utilizing hbs!'
+  })
+});
+
+app.get('/about',(req,res) =>{
+  res.render('about.hbs', {
+    siteTitle: 'About - Zirena',
+    pageTitle: 'About Page'
+  });
+});
+
+/*
+app.get('/maintenance',(req,res) =>{
+  res.render('maintenance.hbs', {
+    siteTitle: 'Maintenance'
+  });
+});
+*/
+
+// /bad - send back JSON with errorMessage
+
+app.get('/bad',(req,res) => {
+  res.send({
+    StatusCode: '404',
+    errorMessage: 'Could not locate file.'
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server is up on port 5000.');
+});
